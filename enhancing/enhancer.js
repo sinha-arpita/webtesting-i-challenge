42ed52c19bc7014dab5a0737b1c32b7f80cc192b
Arpita Sinha <sinha.arpita@gmail.com>
2019-04-10 20:17:11 -0700

    MVP FOR FAIL,SUCCEED AND REPAIR GET FOR STRETCH

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,21 +2,71 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
-
+//When enhancement succeeds
+// The item's enhancement increases by 1.
+// If the item enhancement level is 20, the enhancement level is not changed.
+// The durability of the item is not changed.
 function succeed(item) {
+  
+  if(item.enhancement <= 20){
+    item.enhancement +=1;
+    
+  }
   return { ...item };
 }
 
+//If the item's enhancement is less than 15, the durability of the item is
+//decreased by 5.
+//If the item's enhancement is 15 or more, the durability of the item is
+// decreased by 10.
+//If the item's enhancement level is greater than 16, the enhancement level
+//decreases by 1 (17 goes down to 16, 18 goes down to 17).
 function fail(item) {
+  if (item.enhancement < 15) {
+    item.durability -= 5;
+  } else if (item.enhancement >= 15 && item.enhancement <= 16) {
+    item.durability -= 10;
+  } else if (item.enhancement <= 20) {
+    item.enhancement -= 1;
+  }
   return { ...item };
 }
 
 function repair(item) {
+  item.durability = 100;
   return { ...item };
 }
 
 function get(item) {
-  return { ...item };
+   if(item.enhancement>0){
+      item.name="[+" + item.enhancement +"]"+item.name
+      return { ...item };
+   }
+   return item;
 }
+// After a set of interviews with the potential users of the solution, we gathered the following information
+//about the functionality of the game's enhancing system.
+//Your job is to add tests to ./enhancing/enhancer.spec.js and code to ./enhancing/enhancer.js to implement
+//the requirements; ./enhancing/enhancer.js exports an object that has the following methods:
+
+// a repair(item) method that accepts an item object and returns a new item with the durability restored to 100.
+//This method is the simplest of the three and would be a good starting point on this project.
+// a success(item) method that accepts an item object and returns a new item object
+// modified according to the rules defined by the client for enhancement success.
+// a fail(item) method that accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement failure.
+// a get() method for use when working on the stretch problem.
+// The following sections list information provided by the client about items and the game's enhancing system.
+
+// Items.
+// Items have name, durability and enhancement.
+// The item's enhancement it's a number from 0 to 20.
+// The item's durability it's a number from 0 to 100.
+// 
+// When enhancement fails
+// If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+// If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+// If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
+// Stretch Problem
+//"[+7] Iron Sword".
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,47 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+describe("Item repair/fail/succes/get", () => {
+  describe(" repair for full durability", () => {
+    //arrange
+    const item1 = {
+      name: "sword",
+      enhancement: 7,
+      durability: 54
+    };
+    it("Repair durability", () => {
+      expect(enhancer.repair(item1).durability).toEqual(100);
+      expect(enhancer.fail(item1).durability).toBe(95);
+    });
+  });
+  describe("enhancement fail", () => {
+    const item2 = {
+      name: "arrow",
+      enhancement: 15,
+      durability: 54
+    };
+    it("Fail the item", () => {
+      expect(enhancer.fail(item2).durability).toBe(44);
+    });
+  });
+  describe("enhancement success", () => {
+    const item3 = {
+      name: "bracelet",
+      enhancement: 18,
+      durability: 74
+    };
+    it("Succeed the item", () => {
+      expect(enhancer.fail(item3).enhancement).toBe(17);
+      expect(enhancer.succeed(item3).enhancement).toBe(18);
+    });
+  });
+  describe("enhancement has name impact", () => {
+    const item1 = {
+      name: "sword",
+      enhancement: 7,
+      durability: 54
+    };
+    it("Change name", () => {
+      expect(enhancer.get(item1).name).toEqual("[+7]sword");
+    });
+  });
+});
